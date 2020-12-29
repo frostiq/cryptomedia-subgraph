9d469c89b9b269a0fefbd607d6b15b1cdf521495
Alex Bazhanau <bazhanau@outlook.com>
2020-12-29 01:37:20 -0800

    Rarible alfa integration added

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,5 +1,6 @@
-import { Address, BigInt } from "@graphprotocol/graph-ts"
+import { Address } from "@graphprotocol/graph-ts"
 import { BuilderInstanceCreated } from "../generated/NiftyGateway/NiftyGateway"
+import { TransferSingle, TransferBatch, URI, ERC1155 } from "../generated/Rarible/ERC1155";
 import { NftContract as NftContractTemplate } from "../generated/templates"
 import { ERC721, Transfer } from "../generated/templates/NftContract/ERC721"
 import { NftContract, Nft } from "../generated/schema"
@@ -17,16 +18,56 @@ export function handleBuilderInstanceCreated(event: BuilderInstanceCreated): voi
 
 export function handleTransfer(event: Transfer): void {
   let id = event.address.toHexString() + "/" + event.params.id.toString();
+  let contract = ERC721.bind(event.address);
   let nft = Nft.load(id);
   if (nft == null) {
     nft = new Nft(id);
     nft.contract = event.address.toHexString();
     nft.tokenID = event.params.id;
-    nft.creatorName = fetchCreatorName(event.address);
-    nft.tokenURI = fetchTokenURI(event.address, event.params.id);
+    nft.creatorName = contract.nameOfCreator();
+    nft.tokenURI = contract.tokenURI(event.params.id);
   }
 
   nft.owner = event.params.to;
+  nft.save();
+}
+
+export function handleTransferSingle(event: TransferSingle): void {
+  let address = event.address.toHexString();
+  if (NftContract.load(address) == null) {
+    let nftContract = new NftContract(address);
+
+    // TODO: refactor as Rarible is not ERC721
+    // TODO: handle event.params._value
+    nftContract.name = fetchName(event.address);
+    nftContract.symbol = fetchSymbol(event.address);
+    nftContract.platform = "Rarible";
+    nftContract.save();
+  }
+
+  let id = address + "/" + event.params._id.toString();
+  let contract = ERC1155.bind(event.address);
+  let nft = Nft.load(id);
+  if (nft == null) {
+    nft = new Nft(id);
+    nft.contract = address;
+    nft.tokenID = event.params._id;
+    nft.creatorAddress = contract.creators(event.params._id);
+    nft.tokenURI = contract.uri(event.params._id);
+  }
+
+  nft.owner = event.params._to;
+  nft.save();
+}
+
+export function handleTransferBatch(event: TransferBatch): void { 
+  // TODO: implement
+}
+
+export function handleURI(event: URI): void { 
+  let id = event.address.toHexString() + "/" + event.params._id.toString();
+  let nft = new Nft(id);
+  nft.tokenURI = event.params._value;
   nft.save();
 }
 
@@ -38,14 +79,4 @@ function fetchName(tokenAddress: Address): string {
 function fetchSymbol(tokenAddress: Address): string {
   let contract = ERC721.bind(tokenAddress);
   return contract.symbol();
-}
-
-function fetchCreatorName(tokenAddress: Address): string {
-  let contract = ERC721.bind(tokenAddress);
-  return contract.nameOfCreator();
-}
-
-function fetchTokenURI(tokenAddress: Address, tokenId: BigInt): string {
-  let contract = ERC721.bind(tokenAddress);
-  return contract.tokenURI(tokenId);
 }
