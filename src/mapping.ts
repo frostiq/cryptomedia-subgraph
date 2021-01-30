c89d6b3782a57d51cb1f13f3ddfa5f9d47c318af
Alex Bazhanau <bazhanau@outlook.com>
2021-01-30 15:49:17 -0800

    Ownership entity added, Rarible semi-fungibility support implemented

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,71 +1,33 @@
-import { Address } from "@graphprotocol/graph-ts"
-import { TransferSingle, TransferBatch, URI, ERC1155 } from "../generated/Rarible/ERC1155";
+import { Address, BigInt, store } from "@graphprotocol/graph-ts"
+import { URI } from "../generated/Rarible/ERC1155";
 import { ERC721, Transfer } from "../generated/templates/NftContract/ERC721"
-import { NftContract, Nft } from "../generated/schema"
-import { ZERO_ADDRESS } from "./constants";
+import { Nft, Ownership } from "../generated/schema"
+import { BIGINT_ONE, BIGINT_ZERO, ZERO_ADDRESS } from "./constants";
 
 export function handleTransfer(event: Transfer): void {
   let address = event.address.toHexString();
-  let id = address + "/" + event.params.id.toString();
+  let nftId = address + "/" + event.params.id.toString();
   let contract = ERC721.bind(event.address);
-  let nft = Nft.load(id);
+  let nft = Nft.load(nftId);
   if (nft == null) {
-    nft = new Nft(id);
+    nft = new Nft(nftId);
     nft.contract = address;
     nft.tokenID = event.params.id;
     nft.tokenURI = contract.tokenURI(event.params.id);
     nft.createdAt = event.block.timestamp;
+    nft.save();
   }
+
   if (event.params.to == ZERO_ADDRESS) {
     // burn token
     nft.removedAt = event.block.timestamp;
-  }
-  nft.owner = event.params.to;
-  nft.save();
-}
-
-export function handleTransferSingle(event: TransferSingle): void {
-  let address = event.address.toHexString();
-  if (NftContract.load(address) == null) {
-    let nftContract = new NftContract(address);
-
-    // TODO: refactor as Rarible is not ERC721
-    // TODO: handle event.params._value
-    nftContract.name = fetchName(event.address);
-    nftContract.symbol = fetchSymbol(event.address);
-    nftContract.platform = "Rarible";
-    nftContract.save();
+    nft.save();
   }
 
-  let id = address + "/" + event.params._id.toString();
-  let contract = ERC1155.bind(event.address);
-  let nft = Nft.load(id);
-  if (nft == null) {
-    nft = new Nft(id);
-    nft.contract = address;
-    nft.tokenID = event.params._id;
-    nft.creatorAddress = contract.creators(event.params._id);
-    nft.tokenURI = contract.uri(event.params._id);
-    nft.createdAt = event.block.timestamp;
+  if (event.params.from != ZERO_ADDRESS) {
+    updateOwnership(nftId, event.params.from, BIGINT_ZERO.minus(BIGINT_ONE));
   }
-  if (event.params._to == ZERO_ADDRESS) {
-    // burn token
-    nft.removedAt = event.block.timestamp;
-  }
-
-  nft.owner = event.params._to;
-  nft.save();
-}
-
-export function handleTransferBatch(event: TransferBatch): void { 
-  // TODO: implement
-}
-
-export function handleURI(event: URI): void { 
-  let id = event.address.toHexString() + "/" + event.params._id.toString();
-  let nft = new Nft(id);
-  nft.tokenURI = event.params._value;
-  nft.save();
+  updateOwnership(nftId, event.params.to, BIGINT_ONE);
 }
 
 export function fetchName(tokenAddress: Address): string {
@@ -76,4 +38,29 @@ export function fetchName(tokenAddress: Address): string {
 export function fetchSymbol(tokenAddress: Address): string {
   let contract = ERC721.bind(tokenAddress);
   return contract.symbol();
+}
+
+export function updateOwnership(nftId: string, owner: Address, deltaQuantity: BigInt): void {
+  let ownershipId = nftId + "/" + owner.toHexString();
+  let ownership = Ownership.load(ownershipId);
+
+  if (ownership == null) {
+    ownership = new Ownership(ownershipId);
+    ownership.nft = nftId;
+    ownership.owner = owner;
+    ownership.quantity = BIGINT_ZERO;
+  }
+
+  let newQuantity = ownership.quantity.plus(deltaQuantity);
+
+  if (newQuantity.lt(BIGINT_ZERO)) {
+    throw new Error("Negative token quantity")
+  }
+
+  if (newQuantity.isZero()) {
+    store.remove('Ownership', ownershipId);
+  } else {
+    ownership.quantity = newQuantity;
+    ownership.save();
+  }
 }
