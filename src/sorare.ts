e14103d75dbf6c716b2041a526ee06a714f5a0a2
Alex Bazhanau <bazhanau@outlook.com>
2021-03-01 16:52:11 -0800

    unused ABI removed, code simplified

--- src/sorare.ts
+++ src/sorare.ts
@@ -1,9 +1,6 @@
 import { Transfer } from "../generated/templates/NftContract/ERC721";
 import { Nft, NftContract } from "../generated/schema";
-import { Sorare } from "../generated/Sorare/Sorare";
 import { fetchName, fetchSymbol, handleTransfer } from "./mapping";
-import { Address, BigInt, log } from "@graphprotocol/graph-ts";
-import { SORARE, ZERO_ADDRESS } from "./constants";
 
 export function handleTransferSorare(event: Transfer): void {
   let address = event.address.toHexString();
@@ -20,17 +17,7 @@ export function handleTransferSorare(event: Transfer): void {
   let id = address + "/" + event.params.id.toString();
   let nft = Nft.load(id);
   if (nft.creatorAddress == null) {
-    nft.creatorAddress = getAddressOfCreator(event.address, nft.tokenID);
+    nft.creatorAddress = event.params.to;
     nft.save();
-  }
-}
-
-function getAddressOfCreator(address: Address, tokenId: BigInt): Address {
-  if (address == SORARE) {
-    let contract = Sorare.bind(address);
-    return contract.ownerOf(tokenId);
-  } else {
-    log.warning("Sorare contract address {} not found!", [address.toHexString()]);
-    return ZERO_ADDRESS;
   }
 }
